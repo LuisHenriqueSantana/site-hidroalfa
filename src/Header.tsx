2819d5317eddb0b7a980bb1599017287c0d4a20e
Luis Henrique Santana Paiva <166076350+LuisHenriqueSantana@users.noreply.github.com>
2024-07-26 21:34:49 -0300

    teste

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,27 +1,38 @@
-import "./index.css"
-import img from "./imagens/HidroAlfa.png"
-import "./assets/mobile-navbar"
-import React from "react"
+import React, { useEffect } from "react";
+import MobileNavbar from "./mobileNavbar";
+import img from "./imagens/HidroAlfa.png";
+import "./index.css";
 
+export function Header() {
+  useEffect(() => {
+    const mobileNavbar = new MobileNavbar(
+      ".mobile-menu",
+      ".nav-list",
+      ".nav-list li"
+    );
+    mobileNavbar.init();
+  }, []);
 
-export function Header(){
   return (
     <>
-        <header>
-      <nav>
-        <div className="logo"><a href="/"><img src={img} alt=""/></a></div>
-        <div className="mobile-menu">
-          <div className="line1"></div>
-          <div className="line2"></div>
-          <div className="line3"></div>
-        </div>
-        <ul className="nav-list">
-          <li><a href="#">Início</a></li>
-          <li><a href="#">Sobre</a></li>
-          <li><a href="#">Projetos</a></li>
-          <li><a href="#">Contato</a></li>
-        </ul>
-      </nav>
-    </header>
-    </>)
+      <header>
+        <nav>
+          <div className="logo">
+            <a href="/"><img src={img} alt="" /></a>
+          </div>
+          <div className="mobile-menu">
+            <div className="line1"></div>
+            <div className="line2"></div>
+            <div className="line3"></div>
+          </div>
+          <ul className="nav-list">
+            <li><a href="#">Início</a></li>
+            <li><a href="#">Sobre</a></li>
+            <li><a href="#">Projetos</a></li>
+            <li><a href="#">Contato</a></li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
